37a33a657807f9ad641462d1ff1f3c1f9fb93ae3
BenjaminBakija <benjaminbakija20@gmail.com>
2019-03-18 10:36:41 +0100

    zad2

--- src/ba/unsa/etf/rs/Main.java
+++ src/ba/unsa/etf/rs/Main.java
@@ -3,14 +3,24 @@ package ba.unsa.etf.rs;
 import java.util.Scanner;
 
 public class Main {
+    public static int sumaCifara(int n) {
+        int s=0;
+        while(n!=0) {
+            s=s+n%10;
+            n=n/10;
+        }
+        return s;
+    }
 
     public static void main(String[] args) {
-        int suma;
+        int suma,n;
         Scanner ulaz=new Scanner(System.in);
         System.out.println("Unesite do kojeg broja zelite ispis");
         n=ulaz.nextInt();
-        for(int i=0; i<n; i++) {
-
+        for(int i=1; i<n; i++) {
+            if(i%(sumaCifara(i))==0) {
+                System.out.println(i);
+            }
         }
     }
 }

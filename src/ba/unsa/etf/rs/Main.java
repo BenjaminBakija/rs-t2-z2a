061503fbe3954245d3e1a29e6be6772410f30d70
BenjaminBakija <benjaminbakija20@gmail.com>
2019-03-18 10:23:18 +0100

    Initial commit

---
+++ src/ba/unsa/etf/rs/Main.java
@@ -0,0 +1,17 @@
+package ba.unsa.etf.rs;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int suma;
+        Scanner ulaz=new Scanner(System.in);
+        System.out.println("Unesite do kojeg broja zelite ispis");
+        n=ulaz.nextInt();
+        for(int i=0; i<n; i++) {
+
+        }
+    }
+}
+
